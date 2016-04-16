9c1113240a79ebd55ba645372223d707be590c2c
poojapauskar <git.poojapauskar@gmail.com>
2016-04-16 13:33:15 +0530

    sending mails added

--- users/serializers.py
+++ users/serializers.py
@@ -10,21 +10,28 @@ class UsersSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Users
-        fields = ('pk','name', 'email', 'phone','address','project_description','domain')
+        fields = ('pk','name', 'email', 'phone','project_description','domain')
         #write_only_fields = ('firstame', 'lastname')
 
     def create(self, validated_data):
         """
         Create and return a new `Snippet` instance, given the validated data.
         """
-        import logging
-
-        app.logger.addHandler(logging.StreamHandler(sys.stdout))
-        app.logger.setLevel(logging.ERROR)
         
-        objects=Users.objects.create(name=validated_data.get('name'),email=validated_data.get('email'),phone=validated_data.get('phone'),address=validated_data.get('address'),project_description=validated_data.get('project_description'),doamin=validated_data.get('domain'))
+        objects=Users.objects.create(name=validated_data.get('name'),email=validated_data.get('email'),phone=validated_data.get('phone'),project_description=validated_data.get('project_description'),domain=validated_data.get('domain'))
         # print >> sys.stderr, objects
         
+        msg1="Thank You for contacting Bitjini. We will get back to you soon."
+        msg2=validated_data.get('name')+" with email "+validated_data.get('email')+" and phone "+validated_data.get('phone')+" has contacted Bitjini. Project description is as follows "+validated_data.get('project_description')+" .Interested domain "+validated_data.get('domain')
+
+        from django.core.mail import send_mail
+        send_mail('Bitjini: ',msg1, 'poojapauskar22@gmail.com', [validated_data.get('email')], fail_silently=False)
+
+
+        from django.core.mail import send_mail
+        send_mail('Bitjini: ',msg2, 'poojapauskar22@gmail.com', ['contact@bitjini.com'], fail_silently=False)
+
+
 
 
         return objects
