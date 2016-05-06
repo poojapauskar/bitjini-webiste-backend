5f1e9a7705a2133adf257d624820d4fabbe77c82
poojapauskar <git.poojapauskar@gmail.com>
2016-05-06 16:32:11 +0530

    changed message

--- users/serializers.py
+++ users/serializers.py
@@ -21,17 +21,17 @@ class UsersSerializer(serializers.ModelSerializer):
         objects=Users.objects.create(name=validated_data.get('name'),email=validated_data.get('email'),phone=validated_data.get('phone'),project_description=validated_data.get('project_description'),domain=validated_data.get('domain'))
         # print >> sys.stderr, objects
 
-        if(validated_data.get('domain') == "1"):
-            domain="iOS"
-        if(validated_data.get('domain') == "2"):
-            domain="Android"
-        if(validated_data.get('domain') == "3"):
-            domain="Web"
-        if(validated_data.get('domain') == "4"):
-            domain="Growth Hacking"
+        # if(validated_data.get('domain') == "1"):
+        #     domain="iOS"
+        # if(validated_data.get('domain') == "2"):
+        #     domain="Android"
+        # if(validated_data.get('domain') == "3"):
+        #     domain="Web"
+        # if(validated_data.get('domain') == "4"):
+        #     domain="Growth Hacking"
         
         msg1="Thank You for contacting Bitjini. We will get back to you soon."
-        msg2=validated_data.get('name')+" with email "+validated_data.get('email')+" and phone "+validated_data.get('phone')+" has contacted Bitjini. Project description is as follows "+validated_data.get('project_description')+" .Interested domain "+domain
+        msg2=validated_data.get('name')+" with email "+validated_data.get('email')+" and phone "+validated_data.get('phone')+" has contacted Bitjini."
 
         from django.core.mail import send_mail
         send_mail('Bitjini: ',msg1, 'poojapauskar22@gmail.com', [validated_data.get('email')], fail_silently=False)
